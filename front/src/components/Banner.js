47751865195acfef5273db98baf86816535f0491
wgomg <w.gomez.g@protonmail.ch>
2020-02-28 15:26:58 -0300

    Añadido banner.

--- front/src/components/Banner.js
+++ front/src/components/Banner.js
@@ -5,13 +5,21 @@ import { connect } from 'react-redux';
 import { Loading } from './elements';
 import { getBanners } from '../actions/banners';
 
+import getImageBase64Data from '../utils/getImageBase64Data';
+
 const Banner = ({ board, getBanners, banners: { banners, loading } }) => {
-  // if (board && board.length > 0)
   useEffect(() => {
     getBanners(board[0].board_id);
   }, [getBanners, board]);
 
-  return <div>Banner</div>;
+  const bannerImg = banners.length === 0 ? null : banners[Math.floor(Math.random() * banners.length)];
+
+  const imgSrc = bannerImg ? getImageBase64Data(bannerImg) : null;
+
+  const bannerElement =
+    loading || !imgSrc ? <Loading /> : <img className='banner centered' src={imgSrc} alt='banner' />;
+
+  return <div className='container centered'>{bannerElement}</div>;
 };
 
 Banner.propTypes = {
