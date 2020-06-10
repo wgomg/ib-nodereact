2e8178eb2e868d73257164ca47371cb7cf4231b5
wgomg <w.gomez.g@protonmail.ch>
2020-06-10 02:10:18 -0400

    Añade sanitización para redux-devtools-extesion

--- front/src/store.js
+++ front/src/store.js
@@ -7,6 +7,17 @@ const initState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initState, composeWithDevTools(applyMiddleware(...middleware)));
+const actionSanitizer = (action) =>
+  action.type === 'GET_FILE' && action.payload ? { ...action, payload: '<<LONG_BLOB>>' } : action;
+
+const stateSanitizer = (state) =>
+  state.files.blob ? { ...state, files: { ...state.files, blob: '<<LONG_BLOB>>' } } : state;
+
+const composeEnhancers = composeWithDevTools({
+  actionSanitizer,
+  stateSanitizer,
+});
+
+const store = createStore(rootReducer, initState, composeEnhancers(applyMiddleware(...middleware)));
 
 export default store;
