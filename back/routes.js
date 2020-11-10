c4501cd05d70a6be97a4ee54c3d09976fe9d5b03
wgomg <wgomg@pm.me>
2020-11-10 17:44:03 -0300

    Routes auto-gen refactoring

--- back/routes.js
+++ back/routes.js
@@ -1,140 +1,48 @@
 'use strict';
 
-const models = require('./models');
+const authentication = require('./middlware/auth');
 
-const auth = require('./middlware/auth');
-
-const logger = require('./libraries/logger');
-
-const httpMethods = { s: 'post', u: 'put', g: 'get', d: 'delete' };
-const privateMethods = new Map([
-  ['Ban', ['save']],
-  ['Banner', ['save', 'delete']],
-  ['Board', ['save', 'update', 'delete']],
-  ['Post', []],
-  ['Report', ['getAll', 'updateSolved']],
-  ['Rule', ['save', 'update', 'delete']],
-  [
-    'Staff',
-    ['save', 'get', 'getAuth', 'getAll', 'update', 'updateChangepassword', 'updateResetpassword'],
-  ],
-  ['Tag', ['save', 'update', 'delete']],
-  ['Theme', ['save', 'update', 'delete']],
-  ['Thread', []],
-  ['File', []],
-]);
-
-let privateRoutes = new Map([]);
+const controllers = require('./controllers');
+const Staffs = require('./models/Staffs');
 
 const baseRoute = '/_back/api';
 
-/**
- * Ejemplo: 
-Map(n) {
-  'Board' => [
-    { httpMethod: 'post', fn: 'save', route: '/boards', access: 'priv' },
-    { httpMethod: 'get', fn: 'getByURI', route: '/boards/:uri', access: 'pub' },
-    ...
-  ],
-  'n' => [{...}]
-}
-***/
-const routesMap = new Map(
-  Object.values(models).map((Model) => [
-    Model.name,
-    Model.getFunctions().map((fn) => {
-      const fnName = fn.name;
-      const fnArgs = fn.args;
-      const fnHttpMethod = httpMethods[fnName.charAt(0)];
-
-      let route = `${baseRoute}/${Model.table.toLowerCase()}`;
-      const fnNameArray = fnName
-        .split(/(?=[A-Z])/)
-        .slice(1)
-        .filter((s) => s !== 'All');
-
-      if (fnNameArray.length > 0) route += '/' + fnNameArray.map((p) => p.toLowerCase()).join('/');
-
-      if (
-        fnName !== 'getAuth' &&
-        fnArgs !== null &&
-        (fnHttpMethod === 'get' || fnHttpMethod === 'delete')
-      )
-        route += `/:${fnArgs[0]}`;
-
-      let access = 'pub';
-      if (privateMethods.get(Model.name).includes(fnName)) {
-        access = 'priv';
-        privateRoutes.set(fnHttpMethod.toUpperCase() + route, Model.name + '.' + fn.name);
-      }
-
-      return { httpMethod: fnHttpMethod, fn: fnName, fnArgs, route, access };
-    }),
-  ])
-);
-
 const routes = (app) => {
-  for (let [modelName, endpoints] of routesMap)
-    endpoints.forEach((ep) => app[ep.httpMethod](...appMethodArgs(modelName, ep)));
+  for (let Controller of controllers) {
+    const controllerRoutes = Controller.getRoutes();
+
+    controllerRoutes.forEach((r) => {
+      const { http, route, func, reqProps, auth } = r;
+
+      console.log(http + ': :' + route + ': :' + func);
+
+      let appRouteArgs = [baseRoute + route];
+
+      if (auth.required) appRouteArgs.push(authentication(auth.access, Staffs));
+
+      app[http.toLowerCase()](...appRouteArgs, async (req, res) => {
+        let controllerFuncArgs = [];
+
+        if (reqProps)
+          reqProps.forEach((p) => {
+            if (p.toLowerCase().includes('body'))
+              controllerFuncArgs.push(req.body);
+            else if (p === 'staff') controllerFuncArgs.push(req.staff_id);
+            else controllerFuncArgs.push(req.params[p]);
+          });
+
+        try {
+          const result = await Controller[func](...controllerFuncArgs);
+
+          if (result.errors) res.status(401).json(result.errors);
+          else res.json(result);
+        } catch (error) {
+          console.error(error);
+          res.status(500).send('Server Error');
+        }
+      });
+    });
+  }
 };
-
-const appMethodArgs = (modelName, ep) => {
-  let args = [ep.route];
-
-  if (ep.access === 'priv') args.push(auth(privateRoutes));
-
-  const appMethodCallback = async (req, res) => {
-    if (!req.procId) req.procId = genProcId();
-
-    const Model = models[modelName];
-    Model.procId = req.procId;
-
-    logger.info(`Request ${req.method} ${req.route.path}`, req.procId);
-
-    try {
-      let fnArgs = [];
-
-      if (ep.fnArgs) {
-        const arg = ep.fnArgs[0];
-
-        if (arg === 'body' || arg === 'staff') {
-          if (arg === 'body' && 'file_id' in Model.schema) req[arg].files = req.files || [];
-
-          if ((modelName === 'Post' || modelName === 'Thread') && ep.httpMethod === 'post')
-            req[arg].user = {
-              ipaddress: req.ip,
-              fingerprint: req.fingerprint.hash,
-            };
-
-          fnArgs.push(req[arg]);
-        } else fnArgs.push(req.params[arg]);
-      }
-
-      const result = await Model[ep.fn](...fnArgs);
-
-      if (result.errors) {
-        logger.debug(
-          `[${Model.procId}] Validaton error on ${modelName}.${ep.fn}(), data: ${JSON.stringify(
-            result.errors
-          )}`
-        );
-
-        res.status(401).json(result.errors);
-      } else {
-        logger.debug(`[${Model.procId}] DB: Entries returned ${result.length || result.affectedRows}`);
-        res.json(result);
-      }
-    } catch (error) {
-      logger.error(error);
-      res.status(error.httpStatus || 500).send(error.message || error.sqlMessage || 'Server Error');
-    }
-  };
-
-  args.push(appMethodCallback);
-
-  return args;
-};
-
-const genProcId = () => Math.random().toString(20).substr(2, 6);
 
 module.exports = routes;
